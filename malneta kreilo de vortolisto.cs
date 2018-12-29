321f58224c0c7afc41bdd72dbc9f300d10c58cf7
esperanto2017 <cat1dog@yandex.ru>
2018-12-29 17:28:02 +0500

    Update malneta kreilo de vortolisto.cs
    Endings are being deleted.
    Special words are apart from words from text.

--- malneta kreilo de vortolisto.cs
+++ malneta kreilo de vortolisto.cs
@@ -12,14 +12,46 @@ namespace ParserEsperanto
     {
         static void Main(string[] args)
         {
-            var WordList = new List<string>();
-            AddWordsToList(@"C:\scripts\esperanto\parser\test.txt", WordList);
-            Console.WriteLine("Vortolisto");
-            foreach (var vorto in WordList)
+            var input = new string[] { @"C:\scripts\esperanto\parser\input\Ulrich_Fajron-sentas-mi-interne_RuLit_Me.txt",
+            @"C:\scripts\esperanto\parser\input\Tolkin_La-Mastro-de-l-Ringoj_RuLit_Me.txt",
+            @"C:\scripts\esperanto\parser\input\Lermontov_Rusa-literaturo_3_Princidino-Mary_RuLit_Me.txt",
+            @"C:\scripts\esperanto\parser\input\Dikkens_La-postlasitaj-paperoj-de-la-Klubo-Pikvika_RuLit_Me.txt",
+            @"C:\scripts\esperanto\parser\input\Gashek_Aventuroj-de-la-brava-soldato-Svejk-dum-la-mondmilito_RuLit_Me.txt"};
+            Console.WriteLine("Start");
+            var WordList = new List<string>(); // words will be added, but don't replaced
+            var RadikoList = new List<string>();
+            foreach (var specialaVorto in SpecialWords)
             {
-                Console.WriteLine( vorto);
+                WordList.Add(specialaVorto);
             }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine("Work with " + i);
+                AddWordsToList(input[i], WordList);
+            }
+            Console.WriteLine("Slow Sort");
+            SlowSort(WordList);
             WriteListToFile(@"C:\scripts\esperanto\parser\temp.txt", WordList);
+            Console.WriteLine("Done");
+
+        }
+
+        static void SlowSort(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                char[] arr = list[i].ToCharArray();
+                Array.Reverse(arr);
+                list[i] = new string(arr);
+            }
+            list.Sort();
+            for (int i = 0; i < list.Count; i++)
+            {
+                char[] arr = list[i].ToCharArray();
+                Array.Reverse(arr);
+                list[i] = new string(arr);
+            }
         }
 
         static void AddWordsToList(string path, List<string> list)
@@ -45,6 +77,8 @@ namespace ParserEsperanto
                         {
                             curArr[i] = curArr[i].Substring(0, len - 1) + "o";
                         }
+                        if (curArr[i].Length>1 && !(SpecialWords.Contains(curArr[i])))
+                            curArr[i] = DeleteEnding(curArr[i]); // make it dynamic
                         if (!(list.Contains(curArr[i])))
                             list.Add(curArr[i]);
                     }
@@ -53,15 +87,88 @@ namespace ParserEsperanto
             }
         }
 
+        static string DeleteEnding(string vorto)
+        {
+            var lastChar = vorto[vorto.Length - 1];
+            if ((lastChar == 's' || lastChar == 'j' || lastChar == 'n' && vorto[vorto.Length-2] != 'j') && vorto.Length>2)
+            {
+                return vorto.Substring(0, vorto.Length - 2) + "'";
+            }
+            if (lastChar == 'o' || lastChar == 'a' || lastChar == 'e' || lastChar == 'u' || lastChar =='i' || lastChar == '\'') // ' излишен, но на всякий случай
+            {
+                return vorto.Substring(0, vorto.Length - 1) + "'";
+            }
+            if ((lastChar == 'n' && vorto[vorto.Length - 2] == 'j') && vorto.Length>3)
+            {
+                return vorto.Substring(0, vorto.Length - 3) + "'";
+            }
+            return vorto; // program should ever go here
+        }
+
         static void WriteListToFile(string path, List<string> list)
         {
-            using (StreamWriter sw = new StreamWriter(path, true))
+            using (StreamWriter sw = new StreamWriter(path, false))
             {
+                //Console.WriteLine("Vortolisto"); // should it be input parameter?
                 foreach (var vorto in list)
                 {
-                    sw.WriteLine(vorto);
+                    sw.WriteLine( vorto);
+                    //Console.WriteLine(vorto);
                 }
             }
         }
+
+        static Dictionary<string, string> MakeRhymesDictionary(List<string> wordList)
+        {
+            var rhymesDictionary = new Dictionary<string, string>();
+
+            return rhymesDictionary;
+        }
+
+        static List<string> MakeTempWordList(List<string> wordList) // заменить пары глухих и звонких на одну из пары
+        {
+            var tempList = new List<string>();
+            foreach (var vorto in wordList) // заменять на звонкие
+            {
+                var novaVorto = vorto;
+                novaVorto.Replace('p','b');
+                novaVorto.Replace('f', 'v');
+                novaVorto.Replace('t', 'd');
+                novaVorto.Replace('k', 'g');
+                novaVorto.Replace('h', 'ĥ');
+                novaVorto.Replace('ŝ', 'ĵ');
+            }
+            return tempList;
+        }
+
+
+        /*static readonly string[] SpecialWords1 = new string[] { "ĉu", "mi", "vi", "li", "ŝi", "ĝi", "ni", "ili",
+                    "kaj", "aŭ", "sed", "ĉar", "se", "do", "kvankam", "kvazaŭ", "tamen", "ke",
+                    "kiam", "kiel", "kiom", "kial", "kien", "kies", "ki,",
+                    "tiam", "ĉiam","iam", "ti'", "i'", "ĉi'",
+                    "nenio", "nenie", "neniam",
+                    "jes", "ne", "jam", "ankoraŭ", "ĵus", "nun", "tuj",
+                    "hieraŭ", "hodiaŭ", "morgaŭ", "baldaŭ", "ankaŭ",
+                    "eĉ", "nur", "preskaŭ", "tre", "tro", "ja", "mem", "ambaŭ", "for", "jen", "nul",
+                    "unu'", "du'", "tri'", "kvar'", "kvin'", "ses'", "sep'", "ok'", "naŭ'", "dek'", "cent'", "mil'", // unu du tri will make an error...
+                    "en", "al", "el", "inter", "sub", "sur", "super", "antaŭ", "ĉe", "trans", "tra", "ĉirkaŭ", "ĝis",
+                    "de", "ekster", "apud", "preter", "pri", "kun", "sen", "kontraŭ", "anstataŭ", "post", "per", "laŭ",
+                    "dum", "krom", "malgraŭ", "por", "pro", "po", "da", "je", "la", "pli", "plej" }; */
+        static readonly string[] SpecialWords = new string[] { "ĉu", "mi", "vi", "li", "ŝi", "ĝi", "ni", "ili",
+                "kaj", "aŭ", "sed", "ĉar", "se", "do", "kvankam", "kvazaŭ", "tamen", "ke",
+                "kiam", "kiel", "kiom", "kial", "kien", "kies", "kio", "kiu", "kie", "kia",
+                "tiam", "ĉiam","iam", "tio", "tia", "tie", "tiu", "io", "ia", "iu", "ie", "ĉio","ĉie","ĉia","ĉiu",
+                "nenio", "nenie", "neniam",
+                "jes", "ne", "jam", "ankoraŭ", "ĵus", "nun", "tuj",
+                "hieraŭ", "hodiaŭ", "morgaŭ", "baldaŭ", "ankaŭ", 
+                "eĉ", "nur", "preskaŭ", "tre", "tro", "ja", "mem", "ambaŭ", "for", "jen", "nul",
+                "unu'", "du'", "tri'", "kvar'", "kvin'", "ses'", "sep'", "ok'", "naŭ'", "dek'", "cent'", "mil'", // unu du tri will make an error...
+                "en", "al", "el", "inter", "sub", "sur", "super", "antaŭ", "ĉe", "trans", "tra", "ĉirkaŭ", "ĝis",
+                "de", "ekster", "apud", "preter", "pri", "kun", "sen", "kontraŭ", "anstataŭ", "post", "per", "laŭ",
+                "dum", "krom", "malgraŭ", "por", "pro", "po", "da", "je", "la", "pli", "plej" };
+
+
+
     }
+
 }
